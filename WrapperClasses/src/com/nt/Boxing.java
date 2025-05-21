b45fb79c038e4d256376c1a9d852fe7a831720df
puspanjaliparida <paridapuspanjali22@gmail.com>
2025-05-21 15:36:42 +0530

    update

---
+++ WrapperClasses/src/com/nt/Boxing.java
@@ -0,0 +1,28 @@
+package com.nt;
+
+public class Boxing {
+    public static void main(String[] args) {
+    	/*Integer i1 = new Integer(10);
+        Integer i2 = new Integer(10);
+        System.out.println(i1==i2);
+        
+        Integer i3 = Integer.valueOf(10);
+        Integer i4 = Integer.valueOf(10);
+        System.out.println(i3==i4);
+        
+        Integer i5 = new Integer(10);
+        Integer i6 = Integer.valueOf(10);
+        System.out.println(i5==i6);
+        
+        Byte b1 = Byte.valueOf((byte)150);
+        Byte b2 = Byte.valueOf((byte)150);
+        System.out.println(b1==b2);*/
+        
+        //Integer i = new Integer();  
+        //Integer j = new Integer((long)150);
+        Integer k = new Integer((byte)150);
+        Integer l = new Integer((short)150);
+        Integer m = new Integer('A'); 
+    	Integer n = new Integer((int)10L);
+	}
+}
